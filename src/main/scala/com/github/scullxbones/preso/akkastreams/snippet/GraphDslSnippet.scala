88a271f488974f16abf2fe7bfbbafb5e4ae6aacb
Brian Scully <scullduggery@gmail.com>
2016-05-18 09:46:01 -0400

    Tweak graph dsl example; more slide changes

--- src/main/scala/com/github/scullxbones/preso/akkastreams/snippet/GraphDslSnippet.scala
+++ src/main/scala/com/github/scullxbones/preso/akkastreams/snippet/GraphDslSnippet.scala
@@ -10,12 +10,17 @@ import net.liftweb.http.LiftCometActor
 import scala.concurrent.Future
 
 object GraphDslSnippet extends SampleCometDriver[Done] with Loggable {
+    import concurrent.ExecutionContext.Implicits.global
+
     override def name: String = "graph-dsl"
 
     override def runStream(comet: Box[LiftCometActor]): Future[Done] = {
         val contained = "!!!" :: "@@@" :: Nil
         GraphDslFlows.graph(contained,
             s => comet.foreach(_ ! AddMessages(s :: Nil))
-        )
+        ).map { case(d,l) =>
+            comet.foreach(_ ! AddMessages(s"Total length: $l" :: Nil))
+            d
+        }
     }
 }
--- src/main/scala/com/github/scullxbones/preso/akkastreams/stream/GraphDslFlows.scala
+++ src/main/scala/com/github/scullxbones/preso/akkastreams/stream/GraphDslFlows.scala
@@ -1,35 +1,48 @@
 package com.github.scullxbones.preso.akkastreams.stream
 
-import akka.NotUsed
+import akka.Done
 import akka.stream.FlowShape
 import akka.stream.scaladsl._
 import com.github.scullxbones.preso.akkastreams.App
 
+import scala.concurrent.{Future, Promise}
+
 object GraphDslFlows extends TickTock {
     import App._
 
-    def sample(source: Source[String, NotUsed]): Flow[String, String, NotUsed] = {
-        Flow.fromGraph(GraphDSL.create(source) { implicit builder => src =>
+    def sample[Mat1,Mat2](source: Source[String, Mat1], tap: Sink[Int, Mat2]): Flow[String, String, Mat2] = {
+        Flow.fromGraph(GraphDSL.create(source, tap)(Keep.right) { implicit builder => (src,snk) =>
             import GraphDSL.Implicits._
 
-            val zipWith = builder.add(UnzipWith[String, String, String](s => s.take(2) -> s.drop(2)))
-            val merge = builder.add(Merge[String](3))
+            val bcast = builder.add(Broadcast[String](2))
+            val len = builder.add(Flow[String].map(_.length))
+            val merge = builder.add(Merge[String](2))
 
-            zipWith.out0    ~> merge.in(0)
-            zipWith.out1    ~> merge.in(1)
-            src.out         ~> merge.in(2)
+            bcast.out(0)    ~> len          ~> snk.in
+            bcast.out(1)    ~> merge.in(0)
+            src.out         ~> merge.in(1)
 
-            FlowShape(zipWith.in, merge.out)
+            FlowShape(bcast.in, merge.out)
         })
     }
 
     import collection.immutable.Seq
-    def graph(contained: Seq[String], logToPage: String => Unit) = {
+    def graph(contained: Seq[String], logToPage: String => Unit): Future[(Done,Int)] = {
         val feed = "ABC" :: "abc" :: "de" :: Nil
+        val lengthPromise = Promise[Int]()
+        val lengthCalc = Sink.fold[Int,Int](0)(_ + _)
 
-        tickTock(Source(feed))
-            .via(sample(tickTock(Source(contained))))
-            .runWith(Sink.foreach(logToPage))
+        val done = tickTock(Source(feed))
+            .viaMat(sample(tickTock(Source(contained)), lengthCalc))(Keep.right)
+            .mapMaterializedValue(lengthPromise.completeWith)
+            .toMat(Sink.foreach(logToPage))(Keep.right)
+            .run()
+
+        implicit val ec = nonblockingDispatcher
+        for {
+            d <- done
+            l <- lengthPromise.future
+        } yield (d,l)
     }
 
 }
